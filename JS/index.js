28260130067b64054d08dd976a6186da09c23da0
Henrique Moraes <kikovillamoraes@gmail.com>
2023-11-27 14:02:36 +0000

    .

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var elem = document.getElementsByClassName("filler")[0];
 	var width = 1;
-	var id = setInterval(frame, 50);
+	var id = setInterval(frame, 30);
 	function frame() {
 		if (width >= 10) {
 				clearInterval(id);
