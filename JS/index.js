93ecad55616949563c7fbd4e7a95daf72cb9d3b9
Henrique Moraes <kikovillamoraes@gmail.com>
2023-11-27 14:07:57 +0000

    .

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,22 @@
 window.onload = function() {
-	var elem = document.getElementsByClassName("filler")[0];
+	var elem = document.getElementById("filler");
 	var width = 1;
 	var id = setInterval(frame, 30);
 	function frame() {
 		if (width >= 10) {
-				clearInterval(id);
+			clearInterval(id);
 		} else {
-				width++;
-				elem.style.width = width + '%';
-				var letters = ['l', 'o', 'a', 'd', 'i', 'n', 'g', 'dot1', 'dot2', 'dot3'];
-				for (var i = 0; i < letters.length; i++) {
-						var letter = document.getElementById(letters[i]);
-						if (width > (i+1) * 10) {
-								letter.style.color = '#4caf50'; 
-						} else {
-								letter.style.color = '#90ee90'; 
-						}
+			width++;
+			elem.style.width = width + '%';
+			var letters = ['l', 'o', 'a', 'd', 'i', 'n', 'g', 'dot1', 'dot2', 'dot3'];
+			for (var i = 0; i < letters.length; i++) {
+				var letter = document.getElementById(letters[i]);
+				if (width > (i+1) * 10) {
+					letter.style.color = '#4caf50'; 
+				} else {
+					letter.style.color = '#90ee90'; 
 				}
+			}
 		}
 	}
 }
